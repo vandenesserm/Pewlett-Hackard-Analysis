00c0097093acd5e8ebf4f401b5187cd6732fbc5f
Raquel Vandenesse <rakelmn@.com>
2022-08-17 10:35:05 -0400

    Upload csv files and queries.

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,49 @@
+-- Deliverable 1: The Number of Retiring Employees by Title
+
+SELECT e.emp_no, 
+	e.first_name, 
+	e.last_name,
+	tt.title, 
+	tt.from_date, 
+	tt.to_date 
+INTO retirement_titles
+FROM employees AS e
+INNER JOIN titles AS tt
+ON (e.emp_no = tt.emp_no)
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY emp_no;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no, 
+	first_name, 
+	last_name, 
+	title
+INTO unique_titles
+FROM retirement_titles
+WHERE to_date = ('9999-01-01')
+ORDER BY emp_no, to_date DESC;
+
+-- Number of employees by their most recent job title who are about to retire
+SELECT COUNT(ut.title), ut.title
+INTO retiring_titles
+FROM unique_titles as ut
+GROUP BY title
+ORDER BY COUNT DESC;
+
+
+
+--DELIVERABLE 2: The Employees Eligible for the Mentorship Program
+
+SELECT DISTINCT ON (emp_no) e.emp_no, 
+	e.first_name, 
+	e.last_name, 
+	e.birth_date,
+	de.from_date, 
+	de.to_date, 
+	tt.title
+INTO mentorship_eligibility
+FROM employees as e
+JOIN dept_emp as de ON (e.emp_no = de.emp_no)
+JOIN titles as tt ON (e.emp_no = tt.emp_no)
+WHERE (de.to_date = ('9999-01-01')) AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY emp_no;
